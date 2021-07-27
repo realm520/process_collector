1a54529b013ef34e4ddd7d89d7bc993d54d23c1e
harryub <harryub@ub.com>
2021-07-27 14:18:44 +0800

    add config file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import json
+
+# from psutil import wait_procs
 from process_collector import utils
 from process_collector import process_operations
 
 
+def loadConfig():
+    confFile = "conf/config.json"
+    with open(confFile) as f:
+        conf = json.load(f)
+        print(conf)
+
+
 def main():
+    loadConfig()
     utils.privilege_check()
     process_operations.create_info_file()
 
